764d71b84a067e9406ac4799a5ddf551239d7e56
Julio Labarca <jlabarca25@gmail.com>
2020-05-03 20:39:23 -0400

    Ball Spawner SampleScene

---
+++ Assets/Scripts/Jobs/MovementJob.cs
@@ -0,0 +1,39 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.Jobs;
+
+namespace JLabarca.Jobs
+{
+    [BurstCompile]
+    public struct MovementJob : IJobParallelForTransform
+    {
+        public NativeArray<Vector3> Position;
+        public NativeArray<Vector3> Velocity;
+        public Vector2 Area;
+        public float DeltaTime;
+
+        void IJobParallelForTransform.Execute(int id, TransformAccess t) {
+            var vel = Velocity[id];
+
+            //area limits
+            if (math.abs(t.position.x) > Area.x/2)
+            {
+                if(math.abs(t.position.x + vel.x) > math.abs(t.position.x) )
+                    vel.x *= -1;
+            }
+
+            if (math.abs(t.position.z) > Area.y/2)
+            {
+                if(math.abs(t.position.z + vel.z) > math.abs(t.position.z) )
+                    vel.z *= -1;
+            }
+
+            //movement
+            Velocity[id] = vel;
+            t.position += vel * DeltaTime;
+            Position[id] = t.position;
+        }
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,64 @@
+﻿using JLabarca.Jobs;
+using Unity.Collections;
+using UnityEngine;
+using UnityEngine.Jobs;
+
+namespace JLabarca
+{
+    public class Spawner : MonoBehaviour
+    {
+        [SerializeField] private GameObject actorPrefab;
+        [SerializeField] private Vector2 area;
+        [SerializeField] private int quantity;
+
+        private NativeList<Vector3> _position;
+        private NativeList<Vector3> _velocity;
+        private TransformAccessArray _transform;
+
+        private void Start() {
+            _position = new NativeList<Vector3>(quantity, Allocator.Persistent);
+            _velocity = new NativeList<Vector3>(quantity, Allocator.Persistent);
+            _transform = new TransformAccessArray(quantity);
+            for (var i = 0; i < quantity; i++)
+            {
+                AddActor();
+            }
+        }
+
+        private void AddActor() {
+            var obj = Instantiate(actorPrefab).transform;
+            var randomVector = Random.insideUnitSphere;
+
+            // Over floor position
+            randomVector.y = 0.05f;
+            obj.position = randomVector * 10;
+            _position.Add(Vector3.zero);
+
+            // Only x/z movement
+            randomVector.y = 0;
+            _velocity.Add(randomVector * 4);
+
+            _transform.Add(obj);
+
+        }
+
+        private void Update() {
+
+            var moveJob = new MovementJob {
+                Position = _position.AsDeferredJobArray(),
+                Velocity = _velocity.AsDeferredJobArray(),
+                DeltaTime = Time.deltaTime,
+                Area = area
+            };
+
+            var moveHandler = moveJob.Schedule(_transform);
+            moveHandler.Complete();
+        }
+
+        private void OnDestroy() {
+            _position.Dispose();
+            _velocity.Dispose();
+            _transform.Dispose();
+        }
+    }
+}
